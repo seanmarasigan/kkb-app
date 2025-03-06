de876de3b3a86d35b4efde6a09a4a0c7d262df5d
shun <marasiganshun@gmail.com>
2025-03-06 15:53:49 +0800

    fix total sum

--- app.py
+++ app.py
@@ -115,12 +115,9 @@ def main():
                     f"<p style='text-align: right'>₱{st.session_state.order_lst["price"][i]:,.2f}</p>",
                     unsafe_allow_html=True
                 )
-            
-            st.session_state.order_lst["quantity"] = pd.to_numeric(st.session_state.order_lst["quantity"])
-            st.session_state.order_lst["price"] = pd.to_numeric(st.session_state.order_lst["price"])
-            st.session_state.order_lst["Total"] = st.session_state.order_lst["quantity"] * st.session_state.order_lst["price"]
+
             st.markdown(
-                f"<p style='text-align: right'>₱{st.session_state.order_lst["Total"].sum():,.2f}</p>",
+                f"<p style='text-align: right'>₱{sum(st.session_state.order_lst["price"]):,.2f}</p>",
                 unsafe_allow_html=True
             )
 
